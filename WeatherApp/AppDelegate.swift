860b6c65694136b96690eff823b92b456d437d96
Nikolay Ignatov <ignatov.99@yandex.ru>
2023-03-07 01:46:24 +0300

    add NavigationBar

--- WeatherApp/AppDelegate.swift
+++ WeatherApp/AppDelegate.swift
@@ -13,12 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      
-        window = UIWindow()
-        window?.rootViewController = ViewController()
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.overrideUserInterfaceStyle = .light
         window?.makeKeyAndVisible()
+         
         
         return true
     }
 }
-
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,12 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+final class ViewController: UIViewController {
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        view.backgroundColor = .white
+        setNavigationController()
     }
-
+    
+    private func setNavigationController() {
+        title = "Weather"
+        navigationItem.searchController = searchController
+        searchController.searchBar.placeholder = "City name"
+        searchController.searchBar.delegate = self
+        searchController.searchBar.enablesReturnKeyAutomatically = false
+        searchController.searchBar.returnKeyType = .send
+        
+        let getCurrentLocationButton = UIBarButtonItem(title: nil,
+                                                       image: UIImage(systemName: "location.north.circle"),
+                                                       target: self,
+                                                       action: #selector(getCurrentLocationButtonTapped))
+        navigationItem.rightBarButtonItem = getCurrentLocationButton
+    }
+    
+    @objc func getCurrentLocationButtonTapped() {
+        print(#function)
+    }
+    
 }
 
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+    }
+}
